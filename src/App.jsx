0f61e2dc6a1fb990cd9a018cc44ead0960ff2017
shahab-edraak <shahab@edraaksystems.com>
2023-08-01 15:29:31 +0500

    navbar final desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./index.css";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 
 const App = () => {
   return (
     <div className="store_container">
+      <Navbar />
+      <Hero />
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./Navbar.css";
+import { FaRegUser } from 'react-icons/fa'
+import { BsSearch } from 'react-icons/bs'
+import { AiOutlineHeart } from 'react-icons/ai'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar_logo">
+        <span className="logo_img"><img src={require('../../assets/logo.png')} alt="logo" /></span>
+        <span className="logo_name">Furniro</span>
+      </div>
+      <ul className="navbar_links">
+        <li>Home</li>
+        <li>Shop</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div className="navbar_icons">
+        <button><FaRegUser /></button>
+        <button><BsSearch /></button>
+        <button><AiOutlineHeart /></button>
+        <button><AiOutlineShoppingCart /></button>
+      </div>
+    </div>
+  )
 };
 
 export default Navbar;
