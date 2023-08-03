ab401a2f6aab1e067fcb40814c00a9b24b503799
shahab-edraak <shahab@edraaksystems.com>
2023-08-03 17:16:48 +0500

    shop final

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -237,7 +237,46 @@ const Shop = () => {
           <button className="product_btn">Next</button>
         </div>
       </section>
-      <section className="shop_support"></section>
+      <section className="shop_support">
+        <div className="support_wrapper">
+          <div className="support_data">
+            <span>
+              <img src={require("../../assets/trofy.png")} alt="" />
+            </span>
+            <span>
+              <h4>High Quality</h4>
+              <p>crafted from top materials</p>
+            </span>
+          </div>
+          <div className="support_data">
+            <span>
+              <img src={require("../../assets/done.png")} alt="" />
+            </span>
+            <span>
+              <h4>Warranty Protection</h4>
+              <p>Over 2 years</p>
+            </span>
+          </div>
+          <div className="support_data">
+            <span>
+              <img src={require("../../assets/shipping.png")} alt="" />
+            </span>
+            <span>
+              <h4>Free Shipping</h4>
+              <p>Order over 150 $</p>
+            </span>
+          </div>
+          <div className="support_data">
+            <span>
+              <img src={require("../../assets/call.png")} alt="" />
+            </span>
+            <span>
+              <h4>24 / 7 Support</h4>
+              <p>Dedicated support</p>
+            </span>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
