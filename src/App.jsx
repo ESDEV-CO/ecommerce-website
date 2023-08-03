3c5681689cb65bc2d4f025906b0b0c02b0ff2cd4
shahab-edraak <shahab@edraaksystems.com>
2023-08-03 13:07:15 +0500

    home section change

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import "./index.css";
 import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Products from "./components/Products/Products";
-import Carousel from "./components/Carousel/Carousel";
-import Category from "./components/Category/Category";
 import Footer from "./components/Footer/Footer";
-import Share from "./components/Share/Share";
+import Home from "./Home";
 
 
 const App = () => {
   return (
     <div className="store_container">
       <Navbar />
-      <Hero />
-      <Category />
-      <Products />
-      <Carousel />
-      <Share />
+      <Home />
       <div className="footer_wrapper">
         <Footer />
       </div>
---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from "./components/Hero/Hero";
+import Category from "./components/Category/Category";
+import Products from "./components/Products/Products";
+import Carousel from "./components/Carousel/Carousel";
+import Share from "./components/Share/Share";
+
+const Home = () => {
+    return (
+        <div className='home_page'>
+            <Hero />
+            <Category />
+            <Products />
+            <Carousel />
+            <Share />
+        </div>
+    )
+}
+
+export default Home
