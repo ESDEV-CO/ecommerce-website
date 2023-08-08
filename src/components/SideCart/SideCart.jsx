84160a475afd5563bac94d2d969f7c07431cfcc1
shahab-edraak <shahab@edraaksystems.com>
2023-08-08 13:00:39 +0500

    sidebar back blur

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./SideCart.css";
+import SinglePage from "../SinglePage/SinglePage";
 
 const SideCart = () => {
-  return <div>SideCart</div>;
+  return (
+    <div className="sidecart">
+      {/* <div className="single_page_blur">
+        <SinglePage />
+      </div> */}
+      <h1>shahab</h1>
+    </div>
+  );
 };
 
 export default SideCart;
--- src/screen/SideCartScreen.js
+++ src/screen/SideCartScreen.js
@@ -2,18 +2,25 @@ import React from "react";
 import SideCart from "../components/SideCart/SideCart";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import SinglePage from "../components/SinglePage/SinglePage";
 
 const SideCartScreen = () => {
   return (
-    <div className="store_container">
+    <div className="sidebar_container">
       <Navbar />
-      <SideCart />
+      <div class="sidebar_single">
+        <SinglePage />
+      </div>
+      <div class="sidebar_main">
+        <SideCart />
+      </div>
       <div className="footer_wrapper">
         <Footer />
       </div>
       <div className="rights">
         <h6>2023 furino. All rights reserved</h6>
       </div>
+      <div class="overlay"></div>
     </div>
   );
 };
