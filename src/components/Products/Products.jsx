55b886756c1ffaa69159040e94edfce9443f9b5c
shahab-edraak <shahab@edraaksystems.com>
2023-08-08 19:05:31 +0500

    product dynamic data

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,13 +4,14 @@ import { BiSolidShareAlt } from "react-icons/bi";
 import { MdOutlineCompareArrows } from "react-icons/md";
 import { AiFillHeart } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { ProductsData } from "./ProductsData";
 
 const Products = () => {
   return (
     <div className="products">
       <h2>Our Products</h2>
       <div className="all_products">
-        <Link to="/single" className="product_info">
+        {/* <Link to="/single" className="product_info">
           <img src={require("../../assets/sylthe.png")} alt="product" />
           <div className="product_info_des">
             <h4>Syltherine</h4>
@@ -106,7 +107,28 @@ const Products = () => {
               <h5>Rp 500.000</h5>
             </div>
           </div>
-        </div>
+        </div> */}
+        {ProductsData.map((itm) => {
+          return (
+            <Link
+              to={"/single"}
+              onClick={() => window.scrollTo(100, 100)}
+              className="product_info new"
+            >
+              <img src={itm?.image} alt="product" />
+              <div className="product_info_des">
+                <h4>{itm?.name}</h4>
+                <h6>{itm?.description}</h6>
+                <div className="side_by_side">
+                  <h5>{itm?.full_price}</h5>
+                  <s className={`${itm?.discount_price ? "sale50" : "new"}`}>
+                    {itm?.discount_price}
+                  </s>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
       <button className="show_more">Show More</button>
     </div>
